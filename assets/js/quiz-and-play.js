cf4ee032a712802347b56dfbf6363e3dc67283a2
Kenarina <ken.arina@gmail.com>
2023-09-05 23:33:16 +0200

    Add function to randomly select the wizard's animal

--- assets/js/quiz-and-play.js
+++ assets/js/quiz-and-play.js
@@ -6,20 +6,25 @@ document.addEventListener("DOMContentLoaded", function () {
       if (this.getAttribute("class") === "birds") {
         alert(`Hey, You selected 'Birds'.` );
         selectMyBird();
+        selectWizardsAnima();
       } else if (this.getAttribute("class") === "fish") {
         alert(" Wow!, You selected 'Fish'.");
         selectMyFish ();
+        selectWizardsAnima();
       } else if (this.getAttribute("class") === "mammals") {
         alert(" Interesting! You selected 'Mammals'.");
         selectMyMammal ();
+        selectWizardsAnima();
       } else if (this.getAttribute("class") === "reptiles") {
         alert(" Hmmmm, You selected 'Reptiles'.");
+        selectMyReptile ();
+        selectWizardsAnima();
       }
     })
   }
   selectAnimal();
 })
-
+/*
 function selectAnimal() {
   const birdsArray = ['African Fish Eagle', 'Ostrich', 'Vulture', 'Marabou Stork', 'Grey Crowned Crane', 'Lilac-Breasted Roller', 'Yellow-Billed Stork', 'African Jacana', 'African Penguin', 'African Pygmy Kingfisher', 'African Skimmer', 'Augur Buzzard', 'Black-Bellied Bustard', 'Black-Winged Lapwing', 'Blue-Naped Mousebird', 'Cattle Egret', 'Collared Pratincole', 'Common Bulbul'];
 
@@ -29,7 +34,7 @@ function selectAnimal() {
 
   const reptilesArray = ['Nile Crocodile', 'Tortoise', 'Black Mamba', 'Puff Adder', 'Nile Monitor Lizard', 'Python', 'King Cobra'];
 
-  const predatorLeague = ['African Fish Eagle', 'Augur Buzzard', 'African Pygmy Kingfisher', 'Vulture', 'Nile Perch', 'Catfish', 'Cheetah', 'Spotted Hyena', 'Lion', 'Leopard', 'African Wild Dog'];
+  const predatorLeague = ['African Fish Eagle', 'Augur Buzzard', 'African Pygmy Kingfisher', 'Vulture', 'Nile Perch', 'Catfish', 'Cheetah', 'Spotted Hyena', 'Lion', 'Leopard', 'African Wild Dog', 'Nile Crocodile', 'Python', 'King Cobra'];
 
   const allAnimalsArray = ['African Fish Eagle', 'Ostrich', 'Vulture', 'Marabou Stork', 'Grey Crowned Crane', 'Lilac-Breasted Roller', 'Yellow-Billed Stork', 'African Jacana', 'African Penguin', 'African Pygmy Kingfisher', 'African Skimmer', 'Augur Buzzard', 'Black-Bellied Bustard', 'Black-Winged Lapwing', 'Blue-Naped Mousebird', 'Cattle Egret', 'Collared Pratincole', 'Common Bulbul', 'Nile Perch', 'Tilapia', 'Tigerfish', 'Catfish', 'Silverfish', 'Lungfish', 'African Elephant', 'Black Rhinoceros', 'Cheetah', 'Spotted Hyena', 'Lion', 'African Wild Dog', 'Baboon', 'Hippopotamus', 'Giraffe', 'Zebra', 'Impala', 'Vervet Monkey', 'Meerkat', 'African Buffalo', 'Leopard', 'Gorilla', 'Nile Crocodile', 'Tortoise', 'Black Mamba', 'Puff Adder', 'Nile Monitor Lizard', 'Python', 'King Cobra'];
 
@@ -55,12 +60,12 @@ function selectAnimal() {
 
   console.log(wizardsAnimal);
 
-}
+} */
 
 function selectMyBird () {
-  const birdsArray = ['African Fish Eagle', 'Ostrich', 'Vulture', 'Marabou Stork', 'Grey Crowned Crane', 'Lilac-Breasted Roller', 'Yellow-Billed Stork', 'African Jacana', 'African Penguin', 'African Pygmy Kingfisher', 'African Skimmer', 'Augur Buzzard', 'Black-Bellied Bustard', 'Black-Winged Lapwing', 'Blue-Naped Mousebird', 'Cattle Egret', 'Collared Pratincole', 'Common Bulbul'];
+  const birdsList = ['African Fish Eagle', 'Ostrich', 'Vulture', 'Marabou Stork', 'Grey Crowned Crane', 'Lilac-Breasted Roller', 'Yellow-Billed Stork', 'African Jacana', 'African Penguin', 'African Pygmy Kingfisher', 'African Skimmer', 'Augur Buzzard', 'Black-Bellied Bustard', 'Black-Winged Lapwing', 'Blue-Naped Mousebird', 'Cattle Egret', 'Collared Pratincole', 'Common Bulbul'];
 
- let myBird = birdsArray[Math.floor(Math.random() * birdsArray.length)];
+ let myBird = birdsList[Math.floor(Math.random() * birdsList.length)];
  
  document.getElementById("js-myBird").innerHTML = `You have been assigned: ${myBird}.`;
 }
@@ -83,6 +88,22 @@ function selectMyMammal () {
   
 }
 
+function selectMyReptile () {
+  const reptilesArray = ['Nile Crocodile', 'Tortoise', 'Black Mamba', 'Puff Adder', 'Nile Monitor Lizard', 'Python', 'King Cobra'];
+
+  const myReptile = reptilesArray[Math.floor(Math.random() * reptilesArray.length)];
+
+  document.getElementById("js-myReptile").innerHTML = `You have been assigned: ${myReptile}.`;
+
+}
+
+function selectWizardsAnima() {
+  const allAnimalsArray = ['African Fish Eagle', 'Ostrich', 'Vulture', 'Marabou Stork', 'Grey Crowned Crane', 'Lilac-Breasted Roller', 'Yellow-Billed Stork', 'African Jacana', 'African Penguin', 'African Pygmy Kingfisher', 'African Skimmer', 'Augur Buzzard', 'Black-Bellied Bustard', 'Black-Winged Lapwing', 'Blue-Naped Mousebird', 'Cattle Egret', 'Collared Pratincole', 'Common Bulbul', 'Nile Perch', 'Tilapia', 'Tigerfish', 'Catfish', 'Silverfish', 'Lungfish', 'African Elephant', 'Black Rhinoceros', 'Cheetah', 'Spotted Hyena', 'Lion', 'African Wild Dog', 'Baboon', 'Hippopotamus', 'Giraffe', 'Zebra', 'Impala', 'Vervet Monkey', 'Meerkat', 'African Buffalo', 'Leopard', 'Gorilla', 'Nile Crocodile', 'Tortoise', 'Black Mamba', 'Puff Adder', 'Nile Monitor Lizard', 'Python', 'King Cobra'];
+
+  const wizardsAnimal = allAnimalsArray[Math.floor(Math.random() * allAnimalsArray.length)];
+
+  document.getElementById("js-wizardsAnimal").innerHTML = `The Animal Wizard has chosen: ${wizardsAnimal}.`;
+}
 
 function calculateScore() {
 

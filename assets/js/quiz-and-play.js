90de77beb6c3bcf78169bea403e0753e003448fe
Kenarina <ken.arina@gmail.com>
2023-09-08 03:24:38 +0200

    Added functions to calculate scores and communicate results.

--- assets/js/quiz-and-play.js
+++ assets/js/quiz-and-play.js
@@ -1,150 +1,127 @@
 
 document.addEventListener("DOMContentLoaded", function () {
-  let buttons = document.getElementsByTagName("button");
+    let buttons = document.getElementsByTagName("button");
   for (let button of buttons) {
     button.addEventListener("click", function () {
-      if (this.getAttribute("class") === "birds") {
-        alert(`Hey, You selected 'Birds'.` );
-        selectMyBird();
-        selectWizardsAnima();
-      } else if (this.getAttribute("class") === "fish") {
-        alert(" Wow!, You selected 'Fish'.");
-        selectMyFish ();
-        selectWizardsAnima();
-      } else if (this.getAttribute("class") === "mammals") {
-        alert(" Interesting! You selected 'Mammals'.");
-        selectMyMammal ();
-        selectWizardsAnima();
-      } else if (this.getAttribute("class") === "reptiles") {
-        alert(" Hmmmm, You selected 'Reptiles'.");
-        selectMyReptile ();
-        selectWizardsAnima();
+      if (this.getAttribute("class") === "js-playGame") {
+        alert(`Hey, a random class will selected, from which an animal will be assigned to you.`);
+        selectAnimals();
+        alert("Rank scores for your animal classes will now be calculated");
+        calculateScores();
+
       }
     })
   }
-  selectAnimal();
-})
-/*
-function selectAnimal() {
-  const birdsArray = ['African Fish Eagle', 'Ostrich', 'Vulture', 'Marabou Stork', 'Grey Crowned Crane', 'Lilac-Breasted Roller', 'Yellow-Billed Stork', 'African Jacana', 'African Penguin', 'African Pygmy Kingfisher', 'African Skimmer', 'Augur Buzzard', 'Black-Bellied Bustard', 'Black-Winged Lapwing', 'Blue-Naped Mousebird', 'Cattle Egret', 'Collared Pratincole', 'Common Bulbul'];
+});
 
-  const fishArray = ['Nile Perch', 'Tilapia', 'Tigerfish', 'Catfish', 'Silverfish', 'Lungfish'];
+const birdsArray = ['African Fish Eagle', 'Ostrich', 'Vulture', 'Marabou Stork', 'Grey Crowned Crane', 'Lilac-Breasted Roller', 'Yellow-Billed Stork', 'African Jacana', 'African Penguin', 'African Pygmy Kingfisher', 'African Skimmer', 'Augur Buzzard', 'Black-Bellied Bustard', 'Black-Winged Lapwing', 'Blue-Naped Mousebird', 'Cattle Egret', 'Collared Pratincole', 'Common Bulbul'];
 
-  const mammalsArray = ['African Elephant', 'Black Rhinoceros', 'Cheetah', 'Spotted Hyena', 'Lion', 'African Wild Dog', 'Baboon', 'Hippopotamus', 'Giraffe', 'Zebra', 'Impala', 'Vervet Monkey', 'Meerkat', 'African Buffalo', 'Leopard', 'Gorilla'];
+const fishArray = ['Nile Perch', 'Tilapia', 'Tigerfish', 'Catfish', 'Silverfish', 'Lungfish'];
 
-  const reptilesArray = ['Nile Crocodile', 'Tortoise', 'Black Mamba', 'Puff Adder', 'Nile Monitor Lizard', 'Python', 'King Cobra'];
+const mammalsArray = ['African Elephant', 'Black Rhinoceros', 'Cheetah', 'Spotted Hyena', 'Lion', 'African Wild Dog', 'Baboon', 'Hippopotamus', 'Giraffe', 'Zebra', 'Impala', 'Vervet Monkey', 'Meerkat', 'African Buffalo', 'Leopard', 'Gorilla'];
 
-  const predatorLeague = ['African Fish Eagle', 'Augur Buzzard', 'African Pygmy Kingfisher', 'Vulture', 'Nile Perch', 'Catfish', 'Cheetah', 'Spotted Hyena', 'Lion', 'Leopard', 'African Wild Dog', 'Nile Crocodile', 'Python', 'King Cobra'];
+const reptilesArray = ['Nile Crocodile', 'Tortoise', 'Black Mamba', 'Puff Adder', 'Nile Monitor Lizard', 'Python', 'King Cobra'];
 
-  const allAnimalsArray = ['African Fish Eagle', 'Ostrich', 'Vulture', 'Marabou Stork', 'Grey Crowned Crane', 'Lilac-Breasted Roller', 'Yellow-Billed Stork', 'African Jacana', 'African Penguin', 'African Pygmy Kingfisher', 'African Skimmer', 'Augur Buzzard', 'Black-Bellied Bustard', 'Black-Winged Lapwing', 'Blue-Naped Mousebird', 'Cattle Egret', 'Collared Pratincole', 'Common Bulbul', 'Nile Perch', 'Tilapia', 'Tigerfish', 'Catfish', 'Silverfish', 'Lungfish', 'African Elephant', 'Black Rhinoceros', 'Cheetah', 'Spotted Hyena', 'Lion', 'African Wild Dog', 'Baboon', 'Hippopotamus', 'Giraffe', 'Zebra', 'Impala', 'Vervet Monkey', 'Meerkat', 'African Buffalo', 'Leopard', 'Gorilla', 'Nile Crocodile', 'Tortoise', 'Black Mamba', 'Puff Adder', 'Nile Monitor Lizard', 'Python', 'King Cobra'];
+const predatorLeague = ['African Fish Eagle', 'Augur Buzzard', 'African Pygmy Kingfisher', 'Vulture', 'Nile Perch', 'Catfish', 'Cheetah', 'Marabou Stork', 'Spotted Hyena', 'Lion', 'Leopard', 'African Wild Dog', 'Nile Crocodile', 'Python', 'King Cobra'];
 
-  const myBird = birdsArray[Math.floor(Math.random() * birdsArray.length)];
+const allAnimalsArray = [birdsArray, mammalsArray, reptilesArray, fishArray];
 
-  console.log(myBird);
+const randomArray = allAnimalsArray[Math.floor(Math.random() * allAnimalsArray.length)];
 
-  const myMammal = mammalsArray[Math.floor(Math.random() * mammalsArray.length)];
+const myAnimal = randomArray[Math.floor(Math.random() * randomArray.length)];
 
-  console.log(myMammal);
+const wizardsAnimal = randomArray[Math.floor(Math.random() * randomArray.length)];
 
+function selectAnimals() {
 
-  const myReptile = reptilesArray[Math.floor(Math.random() * reptilesArray.length)];
-
-  console.log(myReptile);
-
-
-  const myFish = fishArray[Math.floor(Math.random() * fishArray.length)];
-
-  console.log(myFish);
-
-  const wizardsAnimal = allAnimalsArray[Math.floor(Math.random() * allAnimalsArray.length)];
-
-  console.log(wizardsAnimal);
-
-} */
-
-function selectMyBird () {
-  const birdsList = ['African Fish Eagle', 'Ostrich', 'Vulture', 'Marabou Stork', 'Grey Crowned Crane', 'Lilac-Breasted Roller', 'Yellow-Billed Stork', 'African Jacana', 'African Penguin', 'African Pygmy Kingfisher', 'African Skimmer', 'Augur Buzzard', 'Black-Bellied Bustard', 'Black-Winged Lapwing', 'Blue-Naped Mousebird', 'Cattle Egret', 'Collared Pratincole', 'Common Bulbul'];
-
- let myBird = birdsList[Math.floor(Math.random() * birdsList.length)];
- 
- document.getElementById("js-myBird").innerHTML = `You have been assigned: ${myBird}.`;
-}
-
-function selectMyFish () {
-  const fishArray = ['Nile Perch', 'Tilapia', 'Tigerfish', 'Catfish', 'Silverfish', 'Lungfish'];
-
-  const myFish = fishArray[Math.floor(Math.random() * fishArray.length)];
-
-  document.getElementById("js-myFish").innerHTML = `You have been assigned: ${myFish}.`;
-  
-}
-
-function selectMyMammal () {
-  const mammalsArray = ['African Elephant', 'Black Rhinoceros', 'Cheetah', 'Spotted Hyena', 'Lion', 'African Wild Dog', 'Baboon', 'Hippopotamus', 'Giraffe', 'Zebra', 'Impala', 'Vervet Monkey', 'Meerkat', 'African Buffalo', 'Leopard', 'Gorilla'];
-
-  const myMammal = mammalsArray[Math.floor(Math.random() * mammalsArray.length)];
-
-  document.getElementById("js-myMammal").innerHTML = `You have been assigned: ${myMammal}.`;
-  
-}
-
-function selectMyReptile () {
-  const reptilesArray = ['Nile Crocodile', 'Tortoise', 'Black Mamba', 'Puff Adder', 'Nile Monitor Lizard', 'Python', 'King Cobra'];
-
-  const myReptile = reptilesArray[Math.floor(Math.random() * reptilesArray.length)];
-
-  document.getElementById("js-myReptile").innerHTML = `You have been assigned: ${myReptile}.`;
-
-}
-
-function selectWizardsAnima() {
-  const allAnimalsArray = ['African Fish Eagle', 'Ostrich', 'Vulture', 'Marabou Stork', 'Grey Crowned Crane', 'Lilac-Breasted Roller', 'Yellow-Billed Stork', 'African Jacana', 'African Penguin', 'African Pygmy Kingfisher', 'African Skimmer', 'Augur Buzzard', 'Black-Bellied Bustard', 'Black-Winged Lapwing', 'Blue-Naped Mousebird', 'Cattle Egret', 'Collared Pratincole', 'Common Bulbul', 'Nile Perch', 'Tilapia', 'Tigerfish', 'Catfish', 'Silverfish', 'Lungfish', 'African Elephant', 'Black Rhinoceros', 'Cheetah', 'Spotted Hyena', 'Lion', 'African Wild Dog', 'Baboon', 'Hippopotamus', 'Giraffe', 'Zebra', 'Impala', 'Vervet Monkey', 'Meerkat', 'African Buffalo', 'Leopard', 'Gorilla', 'Nile Crocodile', 'Tortoise', 'Black Mamba', 'Puff Adder', 'Nile Monitor Lizard', 'Python', 'King Cobra'];
-
-  const wizardsAnimal = allAnimalsArray[Math.floor(Math.random() * allAnimalsArray.length)];
+  document.getElementById("js-myAnimal").innerHTML = `You have been assigned: ${myAnimal}.`;
 
   document.getElementById("js-wizardsAnimal").innerHTML = `The Animal Wizard has chosen: ${wizardsAnimal}.`;
-}
-
-function calculateScore() {
+};
 
 
-}
+// Rank the lists based on their length
+const rankedLists = [birdsArray, mammalsArray, reptilesArray, fishArray].sort((a, b) => b.length - a.length);
 
-/*
-document.querySelector('.js-add-todo-button')
-  .addEventListener('click', () => {
-    addTodo();
+
+/**Assign a rank score based on the index of the animal chosen in their respective animal list from the first 4 lists (higher index =2, lower index =1)
+ */
+let myRankScore = 0
+let wizardsRankScore = 0;
+let mySurvivalScore = 0;
+let wizardSurvivalScore = 0;
+let myTotalScore = 0
+let wizardsTotalScore = 0
+
+function calculateScores() {
+
+  let myRankScore, wizardsRankScore;
+  rankedLists.forEach(list => {
+    let myAnimalIndex = list.indexOf(myAnimal);
+    let wizardsAnimalIndex = list.indexOf(wizardsAnimal);
+    if (myAnimalIndex !== -1) {
+      myRankScore = myAnimalIndex >= list.length / 2 ? 2 : 1;
+    }
+    if (wizardsAnimalIndex !== -1) {
+      wizardsRankScore = wizardsAnimalIndex >= list.length / 2 ? 2 : 1;
+    }
+  });
+  document.getElementById('js-myRankScore').innerHTML = `Your rank score is:  ${myRankScore}`;
+
+  document.getElementById('js-wizardRankScore').innerHTML = `Animal Wizard's rank score is:  ${wizardsRankScore}`;
+
+  /*Assign survival score of 2, otherwise assign a survival score of zero for animal selected from predatorLeague. */
+
+  if (predatorLeague.includes(myAnimal)) {
+    mySurvivalScore = 2;
+  } else {
+    mySurvivalScore = 0;
+  }
+  if (predatorLeague.includes(wizardsAnimal)) {
+    wizardSurvivalScore = 2;
+  } else {
+    wizardSurvivalScore = 0;
+  }
+
+  /*If both selected animals are from the same list, assign a survival score of 2 for higher index and a survival score of 1 for lower index.*/
+
+  rankedLists.forEach(list => {
+    let myAnimalIndex = list.indexOf(myAnimal);
+    let wizardsAnimalIndex = list.indexOf(wizardsAnimal);
+    if (myAnimalIndex !== -1 && wizardsAnimalIndex !== -1) {
+      if (myAnimalIndex > wizardsAnimalIndex) {
+        mySurvivalScore = 2;
+        wizardSurvivalScore = 1;
+      } else {
+        mySurvivalScore = 1;
+        wizardSurvivalScore = 2;
+      }
+    }
   });
 
-function addTodo() {
-  const inputElement = document.querySelector('.js-name-input');
-  const name = inputElement.value;
-*/
+  // Calculate total score for each person
 
-function displayMyScore() {
+  myTotalScore = myRankScore + mySurvivalScore;
+  wizardsTotalScore = wizardsRankScore + wizardSurvivalScore;
 
-}
+  // Communicate results to the user using the DOM
 
-function displayComputerScore() {
+  document.getElementById('js-mySurvivalScore').innerHTML = `Your survival score is:  ${mySurvivalScore}`;
 
-}
+  document.getElementById('js-wizardSurvivalScore').innerHTML = `Animal Wizard's survival score is:  ${wizardSurvivalScore}`;
 
-function displayFeedbackMessage() {
+  document.getElementById('js-wizardsTotalScore').innerHTML = `Animal Wizard's total score is:  ${wizardsTotalScore}`;
 
-}
-
-function calculateWins() {
-
-}
-
-function incrementScores() {
-
-}
-
-function congratulateWinner() {
-
-}
-
-function resetScore() {
-
+  document.getElementById('js-myTotalScore').innerHTML = `Your total score is:  ${myTotalScore}`;
+  
+  if (myTotalScore > wizardsTotalScore) {
+    /*score.wins += 1;*/
+    document.getElementById('js-wins').innerHTML = `Congratulations! You win bragging rights!`;
+  } else if (myTotalScore < wizardsTotalScore) {
+    /*score.losses += 1;*/
+    document.getElementById('js-losses').innerHTML = `Sorry. You lose bragging rights on this one.Try again.`;
+  } else if (myTotalScore === wizardsTotalScore) {
+    /*score.ties += 1;*/
+    document.getElementById('js-ties').innerHTML = `Hhmmm... It's a tie.`;
+  }
 }
